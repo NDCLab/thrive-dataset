558e6a30a93e4e32665103d508f235f22bcb9a22
Lilly LaPlace <llaplace@fiu.edu>
2023-06-23 17:30:47 -0500

    resolving conflicts between eeg_scripts and main

--- code/eeg_timing_test/thrive_stim_timing_test.R
+++ code/eeg_timing_test/thrive_stim_timing_test.R
@@ -13,7 +13,7 @@ library(dplyr)
 #setwd("~/Users/kihossei/Desktop") #set working directory to where your text file (the one that you have triggers and their times) is located.
 path <- ("/Users/kihossei/Documents/GitHub/thrive-dataset/code/eeg_timing_test")
 
-mrkTxt <- readLines(paste(path, "/thrive_system1_blockLevel_triggerLoss_prevention_timing_test_stim.vmrk", sep = "")) # load the .vmrk file into the workspace.
+mrkTxt <- readLines(paste(path, "/thrive_sys1_timingTest_stim.vmrk", sep = "")) # load the .vmrk file into the workspace.
 myDat <- setNames(data.frame(matrix(nrow = length(mrkTxt), ncol = 1)), c("colA")) # creates an empty data frame with a single column and row # = length(mrkTxt)
 
 # This 'for' loop creates a dataframe from the loaded text file. 
@@ -43,7 +43,7 @@ write.csv(newDat,paste(path,proc_fileName, sep = "/", collapse = NULL), row.name
 
 newDat <- read.csv(paste(path,proc_fileName, sep = "/", collapse = NULL))
 
-timeVal <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff")) # creating an empty data frame that will be filled with time difference values!
+practice_dat <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff")) # creating an empty data frame that will be filled with time difference values!
 dVal <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff"))
 
 # If you wana perform the flanker timing test for stimulus presentation run the code below!
@@ -55,7 +55,7 @@ for (i in 1:nrow(newDat)) {
         firstVal <- str2num(gsub(".*,", "", newDat$colC[i-1]))
         diffVal <- secondVal - firstVal
         dVal[1,] <- diffVal
-        timeVal <- rbind(timeVal, dVal)
+        practice_dat <- rbind(practice_dat, dVal)
       }
     }
   } else {
@@ -64,9 +64,58 @@ for (i in 1:nrow(newDat)) {
 }
 
 
-timeVal <- na.omit(timeVal, na.action = "omit")
-mean(timeVal$timeDiff)
-sd(timeVal$timeDiff)
+practice_dat <- na.omit(practice_dat, na.action = "omit")
+mean(practice_dat$timeDiff)
+sd(practice_dat$timeDiff)
+
+
+nonSocial_dat <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff")) # creating an empty data frame that will be filled with time difference values!
+dVal <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff"))
+
+# If you wana perform the flanker timing test for stimulus presentation run the code below!
+for (i in 1:nrow(newDat)) {
+  if (str_detect(newDat$colC[i], "S128")) {
+    if (i != 1){
+      if ( str_detect(newDat$colC[i-1], "S 41")  || str_detect(newDat$colC[i-1], "S 42") || str_detect(newDat$colC[i-1], "S 43") || str_detect(newDat$colC[i-1], "S 44")){
+        secondVal <- str2num(gsub(".*S128,", "", newDat$colC[i]))
+        firstVal <- str2num(gsub(".*,", "", newDat$colC[i-1]))
+        diffVal <- secondVal - firstVal
+        dVal[1,] <- diffVal
+        nonSocial_dat <- rbind(nonSocial_dat, dVal)
+      }
+    }
+  } else {
+    next
+  }
+}
+nonSocial_dat <- na.omit(nonSocial_dat, na.action = "omit")
+mean(nonSocial_dat$timeDiff)
+sd(nonSocial_dat$timeDiff)
+
+
+
+social_dat <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff")) # creating an empty data frame that will be filled with time difference values!
+dVal <- setNames(data.frame(matrix(ncol = 1)), c("timeDiff"))
+
+# If you wana perform the flanker timing test for stimulus presentation run the code below!
+for (i in 1:nrow(newDat)) {
+  if (str_detect(newDat$colC[i], "S128")) {
+    if (i != 1){
+      if ( str_detect(newDat$colC[i-1], "S 51")  || str_detect(newDat$colC[i-1], "S 52") || str_detect(newDat$colC[i-1], "S 53") || str_detect(newDat$colC[i-1], "S 54")){
+        secondVal <- str2num(gsub(".*S128,", "", newDat$colC[i]))
+        firstVal <- str2num(gsub(".*,", "", newDat$colC[i-1]))
+        diffVal <- secondVal - firstVal
+        dVal[1,] <- diffVal
+        social_dat <- rbind(social_dat, dVal)
+      }
+    }
+  } else {
+    next
+  }
+}
+social_dat <- na.omit(social_dat, na.action = "omit")
+mean(social_dat$timeDiff)
+sd(social_dat$timeDiff)
 
 ######## Below is the timing test results for the Flanker task of the thrive study task on March 31, 2023- Egret PC with the Old EEG equipment######
 # stimulus triggers
@@ -96,6 +145,21 @@ sd(timeVal$timeDiff)
 # 11.46667
 #  sd(test$value)
 # 1.781951
+
+
+
+######## Below is the timing test results for the Flanker task of the thrive study task on June 2, 2023- System 2
+# stimulus triggers
+# mean(test$value)
+# 12.31915
+#  sd(test$value)
+# 1.575718
+######## Below is the timing test results for the Flanker task of the thrive study task on April 2, 2023- System 1
+# stimulus triggers
+# mean(test$value)
+# 12.775
+#  sd(test$value)
+# 1.576876
 
 
 
--- materials/psychopy_tasks/readme.md
+++ materials/psychopy_tasks/readme.md
@@ -1,18 +1,7 @@
-# PsychoPy Experiments
+# Task1 Stimuli
 
-### arrow_alert_v0
+### Instructions
+This folder contains the stimuli used to create the task. Replace "Task1" in the header above with a descriptive name for the task; provide project-specific notes below.
 
-This is the main PsychoPy task that is run during data collection. It consists of a practice flanker task followed by three loops: two flanker tasks (social vs. non-social) and one observe-other condition. All conditions are counterbalanced, and counterbalance for a given participant is identified by the REDCap protocol. For more information on the data and variables from this task, please refer to the data dictionaries for this study.
 
-
-### arrow-alert-v0-timingTest_block_level_triggerloss
-
-This is a slightly modified version of the arrow_alert_v0 task. The only difference is that the target arrow size is increased by 10X. This modification allows us to perform timing tests. The block level refers to the fact that we resolve the issue of losing triggers (because of temporary disconnection of the USB ports from the stimuli computer to the trigger box) at the block level. So, when the connection is lost and triggers disappear, we only lose triggers for the current block of the flanker task. Triggers, then, re-appear in the following block. This block-level trigger loss fix is already applied to the main task. 
-
-### arrow-alert-v0 -trial_level_triggerLoss
-
-This is a version of the main task in which we were trying to fix trigger loss issue at the trial level. Due to time limitations, we paused on troubleshooting this version. So, for the THRIVE study, we fix trigger loss issue at the block level, as described above. 
-
-### timestamp-v0
-
-This is a simple task that shows X on the screen and simultaneously plays an audio sound. It enables us to use these visual and audio outputs in to synchronize timing during the social tasks across Zoom, Audacity, and EEG recordings.
+### Project Notes
